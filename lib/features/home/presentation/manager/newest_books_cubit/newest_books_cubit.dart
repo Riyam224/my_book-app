0bb424b92f23cc47dac210eee926322f9fa55289
Riyam224 <cs50riyam221@gmail.com>
2024-09-22 23:58:37 +0300

    add cubit to the app to manage state and method of newest books

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:my_book/core/errors/failures.dart';
+import 'package:my_book/features/home/data/repos/home_repo.dart';
+
+import '../../../data/models/books/books.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold((Failures) {
+      emit(NewestBooksFailure(Failures.errMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
+}
